c0ca8f2d97194d87c6054d41d3101c8726c5ed1b
alish <alishacharya1217@gmail.com>
2026-07-07 18:57:25 +0545

    changes due to build bug fix

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -3,7 +3,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req: Request) {
+export async function POST(req) {
   try {
     const { name, email, subject, projectType, message } = await req.json();
 
